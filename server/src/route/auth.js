9e2c55111e578e1a3a8291e9b3e20a7ed326cacb
Minh Quân <quando2801@gmail.com>
2022-10-20 00:45:22 +0700

    upload image to cloudinary and login with google

---
+++ server/src/route/auth.js
@@ -0,0 +1,14 @@
+
+const authController = require("../controller/AuthController");
+
+
+module.exports = function(app){
+
+    app.route("/register").post(authController.register)
+
+    app.route("/login").post(authController.login)
+
+    app.route("/refreshToken").get(authController.requestRefreshToken)
+
+    app.route("/logout").get( authController.logout)
+}
